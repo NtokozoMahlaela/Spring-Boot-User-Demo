52d4acd08aab18a72186a7517b82c200d223c81c
NtokozoMahlaela <Ntokozomokoena07@gmail.com>
2025-05-15 08:36:39 +0200

    Updated my file project

--- spring-boot-user-demo 1/spring-boot-user-demo/spring-boot-user-demo/src/main/java/com/example/demo/repo/FakeRepoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.model.User;
-
-public interface FakeRepoInterface {
-    String insertUser(long id, String name, String surname);
-    User findUserById(long id);
-    String deleteUser(long id);
-}
--- spring-boot-user-demo 1/spring-boot-user-demo/spring-boot-user-demo/src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-public interface UserService {
-    void addUser(String name, String surname);
-    void removeUser(long id);
-    void getUser(long id);
-}
